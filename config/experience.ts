8f1d5bebc07e215032562c354f176a9ff6aea11c
Nika Cuckiridze <nikacuckiridze47@gmail.com>
2024-01-11 18:20:34 +0400

    feat: add experience

--- config/experience.ts
+++ config/experience.ts
@@ -1,14 +1,21 @@
 export const experience = [
   {
-    logo: "/godzie.jpeg",
-    name: "Godzie Sport Tech",
-    title: "Frontend Developer",
-    duration: "2023 Mar - 2023 Sept",
+    logo: "/petinder.jpeg",
+    name: "Petinder",
+    title: "Fullstack Developer",
+    duration: "2023 Dec - present",
   },
+
   {
     logo: "/savicsoft.jpeg",
     name: "Savicsoft",
     title: "Frontend Developer",
     duration: "2023 Oct - present",
+  },
+  {
+    logo: "/godzie.jpeg",
+    name: "Godzie Sport Tech",
+    title: "Frontend Developer",
+    duration: "2023 Mar - 2023 Sept",
   },
 ];
