37da2fbe4b00f92c0bb6ce63bb329a07427ed9a2
nikaakin <nikacuckiridze47@gmail.com>
2024-11-16 09:20:10 +0400

    add some styles to slider

--- components/Slider/Slider.tsx
+++ components/Slider/Slider.tsx
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
@@ -6,12 +6,17 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import { Navigation, Pagination, Scrollbar } from "swiper/modules";
+import { LIGHT_THEME } from "@/config";
+import { ThemeContext } from "@/context";
 
 export const Slider = ({
   images,
 }: {
   images: { url: string; alt: string }[];
 }) => {
+  const { theme } = useContext(ThemeContext);
+  const s = styles(theme);
+
   return (
     <Fragment>
       <Swiper
@@ -21,11 +26,12 @@ export const Slider = ({
         pagination={{ clickable: true }}
         scrollbar={{ draggable: true }}
         navigation
+        className={s.swiper}
       >
         {images.map(({ alt, url }) => (
           <SwiperSlide
             key={`project-page-${alt}`}
-            className="!h-[300px] md:!h-[420px]  lg:!h-[640px]  xl:!h-[600px] 2xl:!h-[960px]"
+            className="!h-[240px] md:!h-[420px]  lg:!h-[640px]  xl:!h-[600px] 2xl:!h-[960px]"
           >
             <img
               alt={alt}
@@ -38,3 +44,9 @@ export const Slider = ({
     </Fragment>
   );
 };
+
+const styles = (theme: string) => ({
+  swiper: `rounded-md shadow-header  ${
+    theme === LIGHT_THEME ? "shadow-slate-950" : "shadow-gray-600"
+  }`,
+});
